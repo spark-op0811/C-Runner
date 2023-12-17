ae12bafa6a270e377e0c00355ae26fe2124545aa
happy-spark <107300540+happy-spark@users.noreply.github.com>
2023-12-17 22:28:31 +0900

    a, b, c, d 초기 값을 100으로 지정

--- mystic_ov_26.c
+++ mystic_ov_26.c
@@ -3,21 +3,18 @@
 int main() {
     int a;
     int b;
-    int c=2;
-    int d=2;
-    for(a=0;a<1000;a++) {
-        for(b=0;b<1000;b++){
-            for (c=2;c<1000;c++){
-                for (d=2;d<1000;d++) {
-                    
+    int c;
+    int d;
+    for(a=100;a<200;a++) {
+        for(b=100;b<200;b++){
+            for (c=100;c<200;c++){
+                for (d=100;d<200;d++) {
                     if (pow(a,c)-pow(b,d)==2) {
                         printf("|||||||(%d^%d,%d^%d)|||||||",a,c,b,d);
-                
                     }
                 }
             }
-            
+            printf(a, b, c, d);
         }
-        
     }
 }
