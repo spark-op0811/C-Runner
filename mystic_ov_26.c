4a357e1f74cfbc801229c0152167ac4cb1ac2194
daniel081139 <70823522+daniel081139@users.noreply.github.com>
2023-12-17 22:23:35 +0900

    Update mystic_ov_26.c
    more

--- mystic_ov_26.c
+++ mystic_ov_26.c
@@ -5,10 +5,10 @@ int main() {
     int b;
     int c=2;
     int d=2;
-    for(a=0;a<100;a++) {
-        for(b=0;b<100;b++){
-            for (c=2;c<100;c++){
-                for (d=2;d<100;d++) {
+    for(a=0;a<1000;a++) {
+        for(b=0;b<1000;b++){
+            for (c=2;c<1000;c++){
+                for (d=2;d<1000;d++) {
                     
                     if (pow(a,c)-pow(b,d)==2) {
                         printf("|||||||(%d^%d,%d^%d)|||||||",a,c,b,d);
