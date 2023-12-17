931574d4f2f1ee15814349e3f49eb3d07a3fd78f
daniel081139 <70823522+daniel081139@users.noreply.github.com>
2023-12-17 22:09:42 +0900

    Update mystic_ov_26.c
    memreduced

--- mystic_ov_26.c
+++ mystic_ov_26.c
@@ -9,7 +9,7 @@ int main() {
         for(b=0;b<100;b++){
             for (c=2;c<100;c++){
                 for (d=2;d<100;d++) {
-                    printf("%d %d %d %d  %f\n",a,c,b,d,pow(a,c)-pow(b,d));
+                    
                     if (pow(a,c)-pow(b,d)==2) {
                         printf("|||||||(%d^%d,%d^%d)|||||||",a,c,b,d);
                 
