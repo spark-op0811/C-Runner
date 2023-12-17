26e44c3f8636fbdc6e0ff82c73e0a580c9e25791
happy-spark <107300540+happy-spark@users.noreply.github.com>
2023-12-17 22:37:00 +0900

    200 ~ 500

--- mystic_ov_26.c
+++ mystic_ov_26.c
@@ -5,10 +5,10 @@ int main() {
     int b;
     int c;
     int d;
-    for(a=100;a<200;a++) {
-        for(b=100;b<200;b++){
-            for (c=100;c<200;c++){
-                for (d=100;d<200;d++) {
+    for(a=200;a<500;a++) {
+        for(b=200;b<500;b++){
+            for (c=200;c<500;c++){
+                for (d=200;d<500;d++) {
                     if (pow(a,c)-pow(b,d)==2) {
                         printf("|||||||(%d^%d,%d^%d)|||||||",a,c,b,d);
                     }
